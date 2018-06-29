19a8d11dedcd004bec34d0fffc5dc0aa9177abc3
Peppuz <peppuzvitale@gmail.com>
2018-06-30 01:18:04 +0200

    Documentazione aggiunta volta per volta

---
+++ README.md
@@ -0,0 +1,7 @@
+# Server Manager Bot
+
+The meaning of this bot is interfacing CLI to a telegram chat supporting navigation with Inline Buttons.
+From the bot, processes can be stopped and relunch and relunch after a  pull
+
+## License 
+LGPL 
--- server_manager.py
+++ server_manager.py
@@ -1,18 +1,20 @@
 import test
-from functools import wraps
 import telegram
 import logging
 import os
 import json
-from telegram.ext import (Updater,
+from functools import wraps
+from telegram import (
+    Emoji,
+    ForceReply,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup)
+from telegram.ext import (
+    Updater,
     MessageHandler,
     Filters,
     CommandHandler,
     CallbackQueryHandler)
-from telegram import (Emoji,
-    ForceReply,
-    InlineKeyboardButton,
-    InlineKeyboardMarkup)
 from telegram import ReplyKeyboardMarkup as RKM
 
 logging.basicConfig(format='%(asctime)s - %(message)s',
@@ -39,8 +41,13 @@ def restricted(func):
 
 
 def start(b, u):
-    bot.sendMessage(u.message.chat_id,
-                    text='Ciao ' + u.message.from_user.first_name+'!')
+    usr = u.message.from_user
+    if usr.id in config['admins']:
+        text = "Bella %s! \nPWD ~ %s" % (usr.first_name, pwd)
+        bot.send_message(u.message.chat_id, text)
+        # TODO: insert here Callback buttons if id in admin_list 
+        return 
+    bot.send_message(u.message.chat_id,'Ciao ' + usr.first_name+'!')
     for val in config['admins']:
         bot.sendMessage(val, "User: %s \n started ServerManager"
                         % u.message.from_user.username)
@@ -48,28 +55,16 @@ def start(b, u):
 
 @restricted
 def commands(b, u):
+    """ This function handles all type of text messages incoming """ 
     m = u.message
     usr = m.from_user
-    if 'cd' in m.text:
-        try:
-            os.chdir(m.text[3:])
-        except Exception as e:
-            bot.sendMessage(m.chat_id, "There was a lil prob with the command.")
-        pwd = os.popen('pwd').read()
-        bot.sendMessage(
-            m.chat_id,
-            '<b>Folder</b>:\n%s\n%s' % (pwd, os.popen('ls').read()),
-            parse_mode='HTML'
-        )
-        return
-    try:
-        bot.sendMessage(m.chat_id, "%s" % os.popen(m.text).read())
-    except Exception as e:
-        bot.sendMessage(m.chat_id, "Command not recognized")
+    bot.send_message(m.chat_id, "executing.."
+    # TODO: make the CLI do shiet 
 
 
 @restricted
 def ricevi_file(b, u):
+    """ This function waits for a document and stores in config[pwd]"""
     d = u.message.document
     pfile = bot.getFile(d.file_id)
     pfile.download(custom_path=pwd)
@@ -78,24 +73,32 @@ def ricevi_file(b, u):
 
 @restricted
 def send_doc(b, u, args):
-    # Simple usagx
+    """ This function sends the file requested from command /get """ 
     m = u.message
-    print args
     with open(args[0]) as figa:
-        loc = bot.sendDocument(m.chat_id, figa, caption='Pepuuz')
-        online_data.append(loc.message_id)
-    check_online_data(bot, m.chat_id)
+        bot.sendDocument(m.chat_id, figa, caption='Is this the file requested?')
 
 
 if __name__ == '__main__':
     ds = updater.dispatcher
-    # Commands
+
+    """ Commands Interface
+         * start | menu - bot menu 
+         * get - expects file_name as arg 
+         * 
+    """ 
     ds.add_handler(CommandHandler('start', start))
+    ds.add_handler(CommandHandler('menu', start))
     ds.add_handler(CommandHandler('get', send_doc, pass_args=True))
-    # Messages
+
+    # Message to CLI - gets output to less and sends text
     ds.add_handler(MessageHandler(Filters.text, commands))
+        
+    # Incoming files [excluding supported audio or videos] are uploaded into the current pwd
     ds.add_handler(MessageHandler(Filters.document, ricevi_file))
+
     # Callbacks
     ds.add_handler(CallbackQueryHandler(deletedata))
+
     # Start Bot
     updater.start_polling()
